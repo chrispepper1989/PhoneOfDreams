c868039204c31137a1c594da7216c1c09bb50a7f
Christopher Pepper <christopher.pepper@codecomputerlove.com>
2023-02-11 10:09:28 +0000

    Added Models

---
+++ src/game.ts
@@ -0,0 +1,81 @@
+﻿enum Location {
+    Cinema,
+    HighSchool,
+    TownPark,
+    Mall,
+    GamerLounge,
+    Beach,
+}
+enum Sport
+{
+    Baseball,
+    Hocky,
+    Surfing,
+    Football,
+    Tennis,
+    BasketBall
+}
+enum Food
+{
+    IceCream,
+    Fruit,
+    Chocolate,
+    Burgers,
+    Fries,
+    Pizza,
+}
+enum Clothes
+{
+    Jeans,
+    Sneakers,
+    Shades,
+    Jacket,
+    Hat,
+    Orange
+}
+enum Names
+{
+    //Gamer
+    John,
+    Brandon,
+    Mark,
+    Peter,
+    
+    //Beach
+    Michael,
+    David,
+    Josh,
+    Alex,
+    
+    //Buckly
+    Nick,
+    Jack,
+    Tyler,
+    Patrick,
+    
+    //Theater
+    Anthony,
+    James,
+    Will,
+    Matt,
+    
+    //Town Park
+    Tom,
+    Luke,
+    Jason,
+    Ben,
+    
+    //Mall,
+    Adam,
+    Chris,
+    Harry,
+    Paul,
+    
+}
+
+type Boy = {
+    location: Location,
+    sport?: Sport,
+    food?:Food,
+    wears?:Clothes
+};
